2d73b2a7a6c4595efa5125904e9a7343e93c2ead
PiterCapistrano <pitercapistrano@gmail.com>
2023-04-23 15:17:51 -0300

    Aatualização ex3_3.js

--- Cap03/ex3_3.js
+++ Cap03/ex3_3.js
@@ -2,19 +2,8 @@ const prompt = require("prompt-sync")()
 const funcionario = prompt("Funcionário: ")
 const salario = Number(prompt("Salário Mensal R$: "))
 const tempo = Number(prompt("Tempo de Serviço (Anos): "))
-const salarioano = salario * 12
-const anototal = salarioano * tempo
 const quadrientos = Math.floor(tempo / 4)
-<<<<<<< HEAD
-const temporest = Number((tempo * 12) - (quadrientos * 48))
-const acrecimo = salario * quadrientos / 100
+const acrecimo = Number(salario * ((Math.pow((1+0.01), quadrientos))) - salario)
 console.log(`Funcionário: ${funcionario}`)
 console.log(`Quadriênios: ${quadrientos}`)
 console.log(`Salário Final R$: ${(salario+acrecimo).toFixed(2).replace(/\./g, ",")}`)
-console.log(`Total Recebido R$: ${Number((anototal)+(acrecimo*temporest)).toFixed(2).replace(/\./g, ",")}`)
-=======
-const acrecimo = salarioano * quadrientos / 100
-console.log(`Funcionário: ${funcionario}`)
-console.log(`Quadriênios: ${quadrientos}`)
-console.log(`Salário Final R$: ${(anototal+acrecimo).toFixed(2).replace(/\./g, ",")}`)
->>>>>>> 82daff0 (Update ex3_3.js)
